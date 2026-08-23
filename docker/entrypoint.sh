05451545014e2ed2b5f8532507a33a78e4e1986d
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-23 23:55:57 +0430

    Initial delta version

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-if [ "${1:-}" = "gunicorn" ]; then
-  python manage.py migrate --noinput
-  python manage.py collectstatic --noinput
-  python manage.py bootstrap_shop
-fi
-exec "$@"
-
