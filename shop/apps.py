6b49027a5b832155b00fe0577afaa74801514b92
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-26 20:09:47 +0330

    Register extra storefront models

--- shop/apps.py
+++ shop/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
 
+
 class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shop"
     verbose_name = "فروشگاه"
 
+    def ready(self):
+        # These models live in a separate module to keep the original store models stable.
+        from . import extra_models  # noqa: F401
