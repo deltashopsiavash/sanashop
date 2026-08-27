1dfd33bd44d7e1643db333d0b9075ec9d095f134
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-27 10:06:54 +0330

    Route branded reset emails and bot API v7

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from django.urls import reverse_lazy
 
 from . import account_views, views
-from .site_api_v6 import bot_api
+from .auth_views import BrandedPasswordResetView
+from .site_api_v7 import bot_api
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -16,7 +17,7 @@ urlpatterns = [
     path("account/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
         "account/password-reset/",
-        auth_views.PasswordResetView.as_view(
+        BrandedPasswordResetView.as_view(
             template_name="registration/password_reset_form.html",
             email_template_name="registration/password_reset_email.txt",
             html_email_template_name="emails/password_reset.html",
