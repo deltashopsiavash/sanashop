d790e8d575d4167cb0e857a0281c8ece6ef4b75f
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-08-27 09:02:36 +0330

    Use Django password reset mail pipeline

--- shop/emailing.py
+++ shop/emailing.py
@@ -1,12 +1,9 @@
 from html import escape
 
 from django.conf import settings
-from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.forms import PasswordResetForm
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-from django.urls import reverse
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
 
 from .models import SiteSetting
 
@@ -29,29 +26,18 @@ def send_otp_email(user, otp):
     message.send(fail_silently=False)
 
 
-def build_password_reset_url(request, user):
-    uid = urlsafe_base64_encode(force_bytes(user.pk))
-    token = default_token_generator.make_token(user)
-    path = reverse("password_reset_confirm", kwargs={"uidb64": uid, "token": token})
-    return request.build_absolute_uri(path)
-
-
 def send_password_reset_email(request, user):
-    store = SiteSetting.load()
-    reset_url = build_password_reset_url(request, user)
-    subject = f"بازیابی رمز عبور {store.site_name}"
-    text = (
-        f"برای ساخت رمز عبور جدید از لینک زیر استفاده کنید:\n\n{reset_url}\n\n"
-        "اگر شما این درخواست را نداده‌اید، این ایمیل را نادیده بگیرید."
+    form = PasswordResetForm({"email": user.email})
+    if not form.is_valid():
+        raise ValueError("invalid_reset_email")
+    form.save(
+        request=request,
+        use_https=request.is_secure(),
+        from_email=_from_email(),
+        email_template_name="registration/password_reset_email.txt",
+        html_email_template_name="emails/password_reset.html",
+        subject_template_name="registration/password_reset_subject.txt",
     )
-    html = render_to_string(
-        "emails/password_reset.html",
-        {"store": store, "user": user, "reset_url": reset_url},
-    )
-    message = EmailMultiAlternatives(subject, text, _from_email(), [user.email])
-    message.attach_alternative(html, "text/html")
-    message.send(fail_silently=False)
-    return reset_url
 
 
 def send_broadcast_email(subject, body, recipients):
@@ -80,5 +66,6 @@ def send_broadcast_email(subject, body, recipients):
         batch = addresses[index:index + 60]
         message = EmailMultiAlternatives(subject, text, _from_email(), [], bcc=batch)
         message.attach_alternative(html, "text/html")
-        sent += message.send(fail_silently=False) * len(batch)
+        if message.send(fail_silently=False):
+            sent += len(batch)
     return sent
